2d1c40340c8d6426d37bd113269b5e296890043b
fjm2u <fm.job@icloud.com>
2025-06-23 20:00:20 +0900

    refactor: update import paths for utility functions to improve module resolution

--- @/components/ui/tooltip.d.ts
+++
@@ -1,7 +0,0 @@
-import * as TooltipPrimitive from "@radix-ui/react-tooltip";
-declare const TooltipProvider: React.FC<TooltipPrimitive.TooltipProviderProps>;
-declare const Tooltip: React.FC<TooltipPrimitive.TooltipProps>;
-declare const TooltipTrigger: React.ForwardRefExoticComponent<any>;
-declare const TooltipContent: any;
-export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
-//# sourceMappingURL=tooltip.d.ts.map
